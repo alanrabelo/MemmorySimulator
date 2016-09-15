7840a975bb94db6904d83dfdffff4e57b49d6fa7
Alan Rabelo Martins <alanrabelo@icloud.com>
2016-09-15 15:42:20 -0300

    Initial Commit

---
+++ MemorySimulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemorySimulator
+//
+//  Created by Alan Rabelo Martins on 9/15/16.
+//  Copyright © 2016 Alan Rabelo Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
