73c9bfe7b0605b7eae0a94a6d8ada5d75e8f0041
Anne Kariny Silva Freitas <annekarinysilvafreitas@macbook-pro-de-anne.local>
2016-09-22 10:21:00 -0300

    Recebendo as entradas de dados do textfield e salvando em uma classe com atributos estáticos

---
+++ MemorySimulator/MemoryProperties.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryProperties.swift
+//  MemorySimulator
+//
+//  Created by Anne Kariny Silva Freitas on 22/09/16.
+//  Copyright © 2016 Alan Rabelo Martins. All rights reserved.
+//
+
+import Foundation
+
+class MemoryProperties {
+    static var numberOfProcesses = 0
+    static var strategy = "fistFit"
+    static var sizeOfMemory = 0
+    static var spaceForOS = 0
+    static var intervalOfProcessSizeInMemory = 0...1
+    static var intervalOfTimeOfInstantiationOfAProcess = 0...1
+    static var intervalOfDurationOfAProcess = 0...1
+
+}
